cc9a8c3460b055f555f3d39951c9494c2f95fb28
n <2585803+tt5@users.noreply.github.com>
2025-04-25 20:34:12 +0200

    slice

--- first/src/main.zig
+++ first/src/main.zig
@@ -8,16 +8,22 @@ fn fibonacci(n: u16) u16 {
 
 
 pub fn main() !void {
+
+    defer std.debug.print("{}\n", .{fibonacci(10)});
+    defer std.debug.print("{}\n", .{fibonacci(11)});
     std.debug.print("hello2 {s}\n", .{foo.bar});
 
-    const a = [_]u8{ 'a', 'b' };
-    std.debug.print("len: {}\n", .{a.len});
+    const array = [_]u8{ 'a', 'b', 'c' };
+    const slice = array[0..2];
 
-    for (a, 0..) |e, i| {
+    for (array, 0..) |e, i| {
         std.debug.print("{} {c}\n", .{i,e});
     }
 
-    std.debug.print("{}", .{fibonacci(10)});
+    for (slice) |v| {
+        std.debug.print("slice elem: {c}\n", .{v});
+    }
+
 }
 
 const expect = std.testing.expect;
@@ -25,5 +31,3 @@ const expect = std.testing.expect;
 test "it works" {
     try expect(true);
 }
-
-
